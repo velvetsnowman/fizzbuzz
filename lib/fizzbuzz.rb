591ceaca899b51f40f7e840bd9088388707b1ab5
velvetsnowman <daniel.mista@gmail.com>
2018-07-05 15:53:18 +0100

    2nd way to rspec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,13 @@
-def fizzbuzz(number)
-  if number%15 == 0
-    return "fizzbuzz"
-  elsif number%3 == 0
-    return "fizz"
-  elsif number%5 == 0
-    return "buzz"
+class Object
+  def fizzbuzz
+    if self%15 == 0
+      return "fizzbuzz"
+    elsif self%3 == 0
+      return "fizz"
+    elsif self%5 == 0
+      return "buzz"
+    else
+      return self
+    end
   end
 end
